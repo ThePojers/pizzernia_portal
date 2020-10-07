48d7f3238fd57c8b9564f8153ba427f6c7a86be2
Dawid Perzynski <loginosobisty@o2.pl>
2020-10-07 15:34:26 +0200

    Add waiter and Login desing

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
-const Login = () => (
+const Login = () => {
 
-  <div className={styles.component}>
-    <h2>
-      Login View
-    </h2>
-    
-  </div>
+  const classes = useStyles();
 
-);
+  return (
+    <Paper>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="outlined-basic" label="Login" variant="outlined" />
+        <TextField id="outlined-basic" label="Password" type="password" variant="outlined" />
+        <Button variant="contained" color="primary">
+          Sign in
+        </Button>
+      </form>
+    </Paper>
+  );
+
+};
 
 export default Login;
